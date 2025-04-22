e602b068b95b8ae5f8c05e37050eaef94a7c93ab
chintu46306 <crg46306@gmail.com>
2025-04-22 18:05:46 +0530

    fixed listing.js file(schema)

--- models/listing.js
+++ models/listing.js
@@ -10,9 +10,9 @@
 //     image: {
 //         type: String,
 //         filename: String,
-//         url: String
-//     //     default: "https://unsplash.com/photos/person-lying-inside-tent-and-overlooking-mountain-re2LZOB2XvY",
-//     //     set: (v)=> v ==="" ? "https://unsplash.com/photos/person-lying-inside-tent-and-overlooking-mountain-re2LZOB2XvY" : v,
+//         url: String,
+//         default: "https://unsplash.com/photos/person-lying-inside-tent-and-overlooking-mountain-re2LZOB2XvY",
+//          set: (v)=> v ==="" ? "https://unsplash.com/photos/person-lying-inside-tent-and-overlooking-mountain-re2LZOB2XvY" : v,
 //      },
 //     price: Number,
 //     location: String,
@@ -24,25 +24,54 @@
 
 
 
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title: {
+//         type: String,
+//         required: true,
+//     },
+//     description: String,
+//     image: {
+//         type: {
+//             filename: String,
+//             url: String
+//         },
+//         required: true,
+//     },
+//     price: Number,
+//     location: String,
+//     country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
+
+
+// final schema
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: String,
-    image: {
-        type: {
-            filename: String,
-            url: String
-        },
-        required: true,
-    },
-    price: Number,
-    location: String,
-    country: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
