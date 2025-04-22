a640c62686f265713393820bdb252177836784c8
chintu46306 <crg46306@gmail.com>
2025-04-22 18:07:20 +0530

    added: create route for new listing

--- app.js
+++ app.js
@@ -29,15 +29,28 @@ app.get("/",(req,res)=>{
 app.get("/listings", async(req, res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});  
-})
+});
+
+//New Route
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+});
 
 //show route
 app.get("/listings/:id", async(req, res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-})
+});
 
+//Create Route
+app.post("/listings", async (req,res)=>{
+    //let {title, description, image, price, country, location}=req.body;
+    //let listing = req.body.listing;
+     const newListing = new Listing(req.body.listing);
+     await newListing.save();
+     res.redirect("/listings");
+})
 
 // app.get("/testListing", async(req,res)=>{
 //     let sampleListing = new Listing({
